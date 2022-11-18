816eb8428ea75796b158b14c925a0150349dc712
Alexander Maslov <AlexM-96@yandex.ru>
2022-11-18 23:14:49 +0300

    CodeChanged

--- src/Main.java
+++ src/Main.java
@@ -16,40 +16,6 @@ public class Main {
         System.out.println("Сумма на счету после пополнения: " + finalAmount + " рублей");
         System.out.println("Бонусные рубли: " + bonusAmount + " рублей");
         System.out.println("Итоговая сумма на счету клиента: " + finalAmoutWithBonus + " рублей");
-
-        //Проверка суммы пополнения меньше 1000
-        int testInitialAmount = 400;
-        int testAddedAmount = 900;
-        int testFinalAmount = testInitialAmount + testAddedAmount;
-        int testBonusAmount;
-        if (testAddedAmount > 1000) {
-            testBonusAmount = testAddedAmount / 100;
-        } else {
-            testBonusAmount = 0;
-        }
-        int testFinalAmoutWithBonus = testInitialAmount + testAddedAmount + testBonusAmount;
-        System.out.println("Начальная сумма на счету: " + testInitialAmount + " рублей");
-        System.out.println("Сумма пополнения: " + testAddedAmount + " рублей");
-        System.out.println("Сумма на счету после пополнения: " + testFinalAmount + " рублей");
-        System.out.println("Бонусные рубли: " + testBonusAmount + " рублей");
-        System.out.println("Итоговая сумма на счету клиента: " + testFinalAmoutWithBonus + " рублей");
-
-        //Проверка суммы пополнения 1000
-        int SecondTestInitialAmount = 400;
-        int SecondTestAddedAmount = 1000;
-        int SecondTestFinalAmount = SecondTestInitialAmount + SecondTestAddedAmount;
-        int SecondTestBonusAmount;
-        if (SecondTestAddedAmount > 1000) {
-            SecondTestBonusAmount = testAddedAmount / 100;
-        } else {
-            SecondTestBonusAmount = 0;
-        }
-        int SecondTestFinalAmoutWithBonus = SecondTestInitialAmount + SecondTestAddedAmount + SecondTestBonusAmount;
-        System.out.println("Начальная сумма на счету: " + SecondTestInitialAmount + " рублей");
-        System.out.println("Сумма пополнения: " + SecondTestAddedAmount + " рублей");
-        System.out.println("Сумма на счету после пополнения: " + SecondTestFinalAmount + " рублей");
-        System.out.println("Бонусные рубли: " + SecondTestBonusAmount + " рублей");
-        System.out.println("Итоговая сумма на счету клиента: " + SecondTestFinalAmoutWithBonus + " рублей");
     }
 }
 
